42ff5a808b803a2493f20969726aa4056b11dfd0
MariemBasily <mariembasily01@gmail.com>
2024-09-12 15:08:12 +0200

    show it

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  //change
+  //change5
   @override
   Widget build(BuildContext context) {
     entryController.loadPreferences();
