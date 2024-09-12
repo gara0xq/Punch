45858f8ad4929071a41302606710c66923691393
xDawideq <Omar06Osama@gmail.com>
2024-09-12 14:26:40 +0300

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  //as
+  //aaaaaa
   @override
   Widget build(BuildContext context) {
     entryController.loadPreferences();
